6f484b309ab32a7bb1f1f47224ffc5b9b7bfeb31
Julian <jchumtl@gmail.com>
2020-04-28 00:38:04 -0400

    Removing useless node packages and hardcoded MongoDB environment variables

--- services/databaseService.js
+++ services/databaseService.js
@@ -3,11 +3,6 @@
 let mongoose = require("mongoose");
 let Message = require("../models/Message");
 
-let uri = "";
-if (typeof process.env.MONGODB_URI == 'undefined') {
-	uri = "mongodb://heroku_99z78930:68jv9r8i7neovka9e2htsricfc@ds059115.mlab.com:59115/heroku_99z78930";
-} else {
-	uri = process.env.MONGODB_URI;
-}
+let uri = process.env.MONGODB_URI;
 
 mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
